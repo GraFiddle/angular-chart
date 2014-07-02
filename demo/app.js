0ea8d44e46f577f19e7ecd4693f07f187f5fff93
Max Klenk <max@klenk.biz>
2014-07-02 09:41:26 +0200

    add JSON edit directive

---
+++ demo/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('demoApp', ['angularChart', 'json-tree']);
+
+app.controller('Controller', function($scope) {
+  $scope.dataset = window.dataset;
+  $scope.options = {};
+});
