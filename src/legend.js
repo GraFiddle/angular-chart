ff9bcf4f5c70e0457072d81564c28fc2b5b92d6d
Max Klenk <max@klenk.biz>
2015-02-19 13:55:55 +0100

    Bugfix: Render tooltips in front of custom navigation - remove z-index; Fix #49

--- src/legend.js
+++ src/legend.js
@@ -15,12 +15,12 @@
         scope: {
           options: '='
         },
-        template: '<button ng-click="toggleMenu()" class="cn-button {{options.button.size}}" ng-class="options.button.cssClass" style="background: {{options.button.background ? options.button.background : options.background}}; color: {{options.button.color ? options.button.color :options.color}};">{{options.content}}</button>' +
-          '<div class="cn-wrapper {{options.size}} items-{{options.items.length}}" ng-class="{\'opened-nav\': options.isOpen}"><ul>' +
+        template: '<div class="cn-wrapper {{options.size}} items-{{options.items.length}}" ng-class="{\'opened-nav\': options.isOpen}"><ul>' +
           '<li ng-repeat="item in options.items">' +
           '<a ng-hide="item.empty" ng-click="perform(options, item)" ng-class="{\'is-active\': item.isActive}" class="{{item.cssClass}}" title="{{item.title}}" style="background: {{item.background ? item.background : options.background}}; color: {{item.color ? item.color : options.color}};">' +
           '<span>{{item.content}}</span>' +
-          '</a></li></ul></div>',
+          '</a></li></ul></div>' +
+          '<button ng-click="toggleMenu()" class="cn-button {{options.button.size}}" ng-class="options.button.cssClass" style="background: {{options.button.background ? options.button.background : options.background}}; color: {{options.button.color ? options.button.color :options.color}};">{{options.content}}</button>',
         controller: ['$scope', '$element', '$attrs',
           function ($scope, $element, $attrs) {
 
